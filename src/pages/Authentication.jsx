72870da2ba3bc1a0b48342fa120a892b04f6b8ea
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-02-13 10:58:46 -0500

    Agrega autenticacion

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import LoginForm from "../components/LoginForm";
@@ -7,11 +7,63 @@ import RegisterForm from "../components/RegisterForm";
 
 function Authentication() {
   const { type } = useParams();
+  const [notification, setNotification] = React.useState("");
+  const navigate = useNavigate();
+
+  function handleRegister(values) {
+    const user = JSON.parse(localStorage.getItem("users"));
+    const userName = values.get("nombre");
+    const userEmail = values.get("correo");
+    const userPassword = values.get("password");
+
+    user[userEmail] = {
+      username: userName,
+      email: userEmail,
+      password: userPassword,
+    };
+
+    localStorage.setItem("users", JSON.stringify(user));
+    setNotification("Registro exitoso, redireccionando a Inicio de sesion");
+    setTimeout(() => {
+      navigate("/authentication/login");
+      setNotification("");
+    }, 1000);
+  }
+
+  function handleLogin(values) {
+    const users = JSON.parse(localStorage.getItem("users"));
+    const loginUser = users[values.get("correo")];
+
+    if (!loginUser) {
+      setNotification(
+        "El correo ingresado no exite, puede registrase si no lo ha hecho"
+      );
+    } else if (loginUser.password !== values.get("password")) {
+      setNotification(
+        "La contraseña ingresada es incorrecta, intentelo de nuevo"
+      );
+    } else {
+      setNotification("Inicio de sesion exitoso, redireccionando a Home");
+      setTimeout(() => {
+        navigate("/");
+        setNotification("");
+      }, 1000);
+    }
+  }
 
   return (
     <>
       <Header />
-      <main>{type === "login" ? <LoginForm /> : <RegisterForm />}</main>
+      <main>
+        {notification !== "" && (
+          <div className="authNotification">{notification}</div>
+        )}
+        {type === "login" ? (
+          <LoginForm onSubmit={handleLogin} />
+        ) : (
+          <RegisterForm onSubmit={handleRegister} />
+        )}
+      </main>
       <Footer />
     </>
   );
