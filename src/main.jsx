bae1bacf0ea8bcf044bc850d098792038e68d5da
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-02-13 08:34:16 -0500

    Modifica logica que inicializa localstorage por primera vez

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
 
+function populateLocalStorage() {
+  const cartList = JSON.parse(localStorage.getItem("cartList"));
+  const login = JSON.parse(localStorage.getItem("login"));
+  const users = JSON.parse(localStorage.getItem("users"));
+  if (cartList === null) {
+    localStorage.setItem("cartList", JSON.stringify({}));
+  }
+
+  if (login === null) {
+    localStorage.setItem("login", "false");
+  }
+
+  if (users === null) {
+    localStorage.setItem("users", JSON.stringify({}));
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>
 );
 
-localStorage.setItem("cartList", JSON.stringify({}));
+populateLocalStorage();
