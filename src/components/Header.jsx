6b5bbb3435d39145c8ccd19b170bd30daeb353fe
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-02-14 15:13:42 -0500

    Agrega logo a header con link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import HamburguerMenu from "./HamburguerMenu";
 import ShoppinCart from "./ShoppingCart";
 import getCartItems from "../javascript/getCartItems";
 import data from "../assets/data.json";
+import logo from "../assets/img/logo/logoMinimalista.png";
 
 function Header() {
   const [cartList, setCartList] = React.useState(null);
@@ -18,7 +20,12 @@ function Header() {
   return (
     <header>
       <HamburguerMenu />
-      <p>Art Delish</p>
+      <div className="logoContainer">
+        <Link to="/">
+          <img src={logo} alt="" />
+          <span>Arts Delish</span>
+        </Link>
+      </div>
       {cartList && <ShoppinCart itemList={cartList} />}
     </header>
   );
