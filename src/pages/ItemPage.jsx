c3a64f53f54fdbddc345374e8671edd7b77c571d
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-02-15 17:50:13 -0500

    Modifica estructura de contenido en ItemPage

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -5,8 +5,10 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Button from "../components/Button";
 import data from "../assets/data.json";
-import ShoppinCart from "../components/ShoppingCart";
 import getCartItems from "../javascript/getCartItems";
+import WideNav from "../components/WideNav";
+import ImgCarousell from "../components/ImgCarousell";
+import SpanIcon from "../components/SpanIcon";
 
 function saveInLocalStorage(id, quantity) {
   const cartList = JSON.parse(localStorage.getItem("cartList"));
@@ -41,31 +43,39 @@ function ItemPage({ saveToCart }) {
   const itemData = itemMap.get(id) ? itemMap.get(id) : null;
   return (
     <>
-      <Header />
-      <ShoppinCart itemList={cartList} />
+      <Header initialCartList={cartList} />
+      <WideNav />
       {itemData ? (
-        <main>
-          <div className="itemImg">
-            <img src={itemData.img} alt="" />
+        <main className="itemInfoContainer">
+          <div className="imgCarousellContainer">
+            <ImgCarousell imgList={itemData.imgList} />
           </div>
-          <div className="itemInformation">
-            <h1>{itemData.cardTitle}</h1>
-            <p>{itemData.longDescription}</p>
-          </div>
-          <div className="addTocart">
-            <label htmlFor="quantity">
-              Cantidad
-              <input
-                id="quantity"
-                onChange={(e) => setQuantity(e.target.value)}
-                type="number"
-                min={0}
-                max={999}
-              />
-            </label>
-            <Button type="button" className="" onClick={() => handleClick()}>
-              Añadir al carro
-            </Button>
+          <div className="itemContainer">
+            <div className="itemInformation">
+              <h1>{itemData.cardTitle}</h1>
+              <p>{itemData.longDescription}</p>
+              <p>
+                Precio:
+                <span>{` $${itemData.price}`}</span>
+              </p>
+            </div>
+            <div className="addToCart">
+              <label htmlFor="quantity">
+                Cantidad
+                <input
+                  id="quantity"
+                  onChange={(e) => setQuantity(e.target.value)}
+                  type="number"
+                  placeholder="1"
+                  min={0}
+                  max={999}
+                />
+              </label>
+              <Button type="button" className="" onClick={() => handleClick()}>
+                <SpanIcon iconName="add_shopping_cart" />
+                Añadir al carro
+              </Button>
+            </div>
           </div>
         </main>
       ) : (
