3bb5ef0456ecf3110b82fcf98a7939eed2b0ef72
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2024-02-16 11:53:51 -0500

    Modifica logica de checkout y aspecto

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -66,17 +66,21 @@ function Checkout() {
       setCreditCardAdded(false);
     }, 100);
   }
+  const hola = { hola: 2 };
 
   if (cartList.size > 0) {
-    const itemsPriceArray = Array.from(cartList.values()).map(
-      (item) => item.price
-    );
+    const itemsPriceArray = Array.from(cartList.values()).map((item) => ({
+      price: item.price,
+      quantity: item.quantity,
+    }));
     totalPrice = itemsPriceArray.reduce((accumulator, currentItem) => {
       const value =
         Number(currentItem.price) * Number(currentItem.quantity) + accumulator;
       return value;
-    });
+    }, 0);
     taxValue = Number(totalPrice) * 0.07;
+    taxValue = Math.round((taxValue + Number.EPSILON) * 100) / 100;
+    totalPrice = Math.round((totalPrice + Number.EPSILON) * 100) / 100;
   }
 
   return (
